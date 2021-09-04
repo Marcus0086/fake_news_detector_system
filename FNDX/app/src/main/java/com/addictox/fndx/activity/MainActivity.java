b0049e75bc2162016b82d4a574c9222c495a9688
PriyanshuG13 <priyanshu.gupta1312@gmail.com>
2021-09-04 15:23:50 +0530

    Logo And SplashScreenActivity: Priyanshu

--- FNDX/app/src/main/java/com/addictox/fndx/activity/MainActivity.java
+++ FNDX/app/src/main/java/com/addictox/fndx/activity/MainActivity.java
@@ -1,9 +1,7 @@
 package com.addictox.fndx.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
-
 import com.addictox.fndx.R;
 
 public class MainActivity extends AppCompatActivity {
@@ -12,5 +10,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
     }
 }
